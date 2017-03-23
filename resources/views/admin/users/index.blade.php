ed779ac69bd810deed5a7a0dda150ab65f1ea949
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-03-23 12:01:38 -0300

    BS-6 Filtro de data.

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -17,9 +17,19 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
+                    <!--Data inicial -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('initial_date', 'Data inicial') !!}
+                        {!! Form::date('initial_date', date('Y-m-d'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <!--Data final -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('final_date', 'Data final') !!}
+                        {!! Form::date('final_date', date('Y-m-d', strtotime('+1 week', strtotime(date('Y-m-d')))), ['class' => 'form-control']) !!}
+                    </div>
                     @include('admin.users.table')
             </div>
         </div>
     </div>
 @endsection
-
--- resources/views/admin/users/table.blade.php
+++ resources/views/admin/users/table.blade.php
@@ -1,5 +1,16 @@
-{!! $dataTable->table(['width' => '100%','class'=>'table table-striped table-hover', true]) !!}
+{!! $dataTable->table(['width' => '100%','class'=>'table table-striped table-hover'], true) !!}
 
 @section('scripts')
     {!! $dataTable->scripts() !!}
+    <script type="text/javascript">
+        var oTable = null;
+
+        $('#initial_date').bind('change', function () {
+            $("#dataTableBuilder").DataTable().draw();
+        });
+
+        $('#final_date').bind('change', function () {
+            $("#dataTableBuilder").DataTable().draw();
+        });
+    </script>
 @endsection
