91c4f29784b1e5597739f83c93916ae547664598
Raul Martinez <raul.martinez@fcmoreno.com.br>
2017-04-04 17:18:17 -0300

    BS-19

---
+++ resources/views/admin/import/checkIn.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <h1 class="pull-left">Validações de colunas</h1>
+    </section>
+    <div class="content">
+        <div class="clearfix"></div>
+
+        @include('flash::message')
+
+
+    </div>
+@endsection
