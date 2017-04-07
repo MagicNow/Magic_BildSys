0efca9d86257627e3ff5ef26b5fb8c3823fb2115
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-04-07 10:55:08 -0300

    BS-34 Inclusão do dashboard no menu do front.

--- resources/views/ordem_de_compras/index.blade.php
+++ resources/views/ordem_de_compras/index.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">Ordem De Compras</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('ordemDeCompras.create') !!}">
-            {{ ucfirst( trans('common.new') )}}
-           </a>
-        </h1>
+        <h1 class="pull-left">Listagem de OC</h1>
     </section>
     <div class="content">
         <div class="clearfix"></div>
@@ -17,7 +12,7 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('ordem_de_compras.table')
+                @include('ordem_de_compras.table')
             </div>
         </div>
     </div>
