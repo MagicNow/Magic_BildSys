d0bd21ac66cd377e76701975c6bdac5b913f2289
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-04-17 17:23:19 -0300

    BS-36 Listando total de itens.

--- app/Http/Controllers/API/ListagemOCController.php
+++ app/Http/Controllers/API/ListagemOCController.php
@@ -9,6 +9,7 @@ use App\Repositories\CodeRepository;
 use Flash;
 use Illuminate\Http\Request;
 use Response;
+use DB;
 
 class ListagemOCController extends AppBaseController
 {
@@ -26,7 +27,32 @@ class ListagemOCController extends AppBaseController
         $listagem_oc = CodeRepository::filter($listagem_oc, $request->all(), $filters_find);
 
         $listagem_oc = $listagem_oc
-            ->select(['ordem_de_compras.id', 'obras.nome as obra', 'users.name as usuario', 'oc_status.nome as situacao'])
+            ->select([
+                    'ordem_de_compras.id',
+                    'obras.nome as obra',
+                    'users.name as usuario',
+                    'oc_status.nome as situacao',
+                    DB::raw('(
+                        SELECT
+                            SUM(qtd)
+                        FROM
+                            ordem_de_compra_itens
+                        WHERE
+                            ordem_de_compra_itens.ordem_de_compra_id = ordem_de_compras.id
+                        AND 
+                            ordem_de_compra_itens.deleted_at IS NULL
+                    ) AS total_comprado'),
+                    DB::raw('(
+                        SELECT
+                            SUM(qtd_total)
+                        FROM
+                            orcamentos
+                        WHERE
+                            orcamentos.obra_id = ordem_de_compras.obra_id
+                        AND
+                            orcamentos.orcamento_tipo_id = 1
+                    ) AS total_para_comprar')
+                ])
             ->join('obras', 'obras.id', '=', 'ordem_de_compras.obra_id')
             ->join('oc_status', 'oc_status.id', '=', 'ordem_de_compras.oc_status_id')
             ->join('users', 'users.id', '=', 'ordem_de_compras.user_id')
