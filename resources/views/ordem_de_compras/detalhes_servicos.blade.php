960f6f7e16b45a7db44f09a58cf83841467c50f5
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-04-24 16:31:40 -0300

    Adicionando responsável na listagem de servicos.

--- resources/views/ordem_de_compras/detalhes_servicos.blade.php
+++ resources/views/ordem_de_compras/detalhes_servicos.blade.php
@@ -97,7 +97,7 @@
                 <div class="panel-body">
                     <div class="col-md-10">
                         <h4 class="highlight">{{ $item->insumo->codigo . ' - '. $item->insumo->nome }}
-                            <a href="/ordens-de-compra/detalhes/{{ $item->ordem_de_compra_id }}" style="font-size:15px;">OC: {{$item->ordem_de_compra_id . ' - Obra: '. $item->obra->nome}}</a>
+                            <a href="/ordens-de-compra/detalhes/{{ $item->ordem_de_compra_id }}" style="font-size:15px;">OC: {{$item->ordem_de_compra_id . ' - Obra: '. $item->obra->nome . ' - Responsável: '. $item->ordemDeCompra->user->name }}</a>
                         </h4>
                     </div>
                     <div class="col-md-2 text-right">
