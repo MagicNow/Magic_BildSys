aae51d2337d3085ec5a25b7fdca5bb2f43e4c20d
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-04-25 15:39:48 -0300

    BS-35 Listagem e porcentagem dinâmica das alçadas.

--- resources/views/ordem_de_compras/detalhe.blade.php
+++ resources/views/ordem_de_compras/detalhe.blade.php
@@ -115,26 +115,34 @@
                 <div class="panel panel-default panel-body">
                     <h4 class="highlight">Timeline</h4>
                     @if(count($avaliado_reprovado))
-                        <h4 class="col-md-4 col-sm-4" style="padding-right: 1px;padding-left: 1px;">
+                        @php  $col_md = 12 / (count($avaliado_reprovado) + 1); @endphp
+                        <h4 class="col-md-{{$col_md}} col-sm-{{$col_md}}" style="padding-right: 1px;padding-left: 1px;">
                             <span>Criação</span>
                             <div class="progress">
                                 <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
                                     100%
                                 </div>
                             </div>
-                        </h4>
                         @foreach($avaliado_reprovado as $alcada => $workflow)
-                            <h4 class="col-md-4 col-sm-4" style="padding-right: 1px;padding-left: 1px;">
-                                <span>{{$alcada}}ª alçada</span>
-                                @if($alcada == count($avaliado_reprovado))
-                                    <span class="pull-right">Finalizado</span>
-                                @endif
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                        100%
+                            @if($workflow['total_avaliado'] && $workflow['aprovadores'])
+                                @php
+                                    $avaliado_aprovadores = $workflow['total_avaliado'] / $workflow['aprovadores'];
+                                    $percentual_quebrado = $avaliado_aprovadores / $qtd_itens;
+                                    $percentual = $percentual_quebrado * 100;
+                                @endphp
+
+                                <h4 class="col-md-{{$col_md}} col-sm-{{$col_md}}" style="padding-right: 1px;padding-left: 1px;">
+                                    <span>{{$alcada}}ª alçada</span>
+                                    @if($alcada == count($avaliado_reprovado))
+                                        <span class="pull-right">Finalizada</span>
+                                    @endif
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="{{$percentual}}" aria-valuemin="0" aria-valuemax="100" style="width: {{$percentual}}%;">
+                                            {{number_format($percentual, 0)}}%
+                                        </div>
                                     </div>
-                                </div>
-                            </h4>
+                                </h4>
+                            @endif
                         @endforeach
                     @else
                         <h4 class="col-md-12 col-sm-12" style="padding-right: 1px;padding-left: 1px;">
