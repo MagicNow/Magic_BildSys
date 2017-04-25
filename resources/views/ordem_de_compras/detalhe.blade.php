3acd38d08415572121192f5a5db7a1c6e0aba2c4
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-04-25 09:40:37 -0300

    BS-35 Layout de timeline.

--- resources/views/ordem_de_compras/detalhe.blade.php
+++ resources/views/ordem_de_compras/detalhe.blade.php
@@ -111,6 +111,31 @@
                 <p class="form-control input-lg">{!! $ordemDeCompra->user->name !!}</p>
             </div>
 
+            <h4 class="col-md-4 col-sm-4" style="padding-right: 1px;padding-left: 1px;">
+                <span>Criação</span>
+                <div class="progress active">
+                    <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                        100%
+                    </div>
+                </div>
+            </h4>
+            <h4 class="col-md-4 col-sm-4" style="padding-right: 1px;padding-left: 1px;">
+                <span>1ª alçada</span>
+                <div class="progress active">
+                    <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                        100%
+                    </div>
+                </div>
+            </h4>
+            <h4 class="col-md-4 col-sm-4" style="padding-right: 1px;padding-left: 1px;">
+                <span>2ª alçada</span>
+                <span class="pull-right">Finalizado</span>
+                <div class="progress active">
+                    <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                        100%
+                    </div>
+                </div>
+            </h4>
         </div>
         <hr>
         <div class="row" id="totalInsumos">
