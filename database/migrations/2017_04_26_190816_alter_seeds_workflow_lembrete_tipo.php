509f330c03a48cdc62d785fbf6dfe7236c770b2f
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-04-26 19:14:13 -0300

    Adequações nos tipos de Lembrete e Tipos de Workflows

---
+++ database/migrations/2017_04_26_190816_alter_seeds_workflow_lembrete_tipo.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterSeedsWorkflowLembreteTipo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $lembreteTipoSeeder = new LembreteTipoTableSeeder();
+        $lembreteTipoSeeder->run();
+
+        $workflowTipoSeeder = new WorkflowTipoTableSeeder();
+        $workflowTipoSeeder->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
