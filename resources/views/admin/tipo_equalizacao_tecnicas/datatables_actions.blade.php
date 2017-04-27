b74c7b1f4837f9d62bad8b2c95bcd641857561b7
Raul Martinez <raul.martinez@fcmoreno.com.br>
2017-04-27 14:18:46 -0300

    BS-100 [Equalização Técnica] - Itens de equalização técnica, listagem, inserção, alteração e exclusão

--- resources/views/admin/tipo_equalizacao_tecnicas/datatables_actions.blade.php
+++ resources/views/admin/tipo_equalizacao_tecnicas/datatables_actions.blade.php
@@ -1,8 +1,8 @@
 {!! Form::open(['route' => ['admin.tipoEqualizacaoTecnicas.destroy', $id], 'id'=>'formDelete'.$id, 'method' => 'delete']) !!}
 <div class='btn-group'>
-    <a href="{{ route('admin.tipoEqualizacaoTecnicas.show', $id) }}" title="{{ ucfirst( trans('common.show') )}}" class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-eye-open"></i>
-    </a>
+    {{--<a href="{{ route('admin.tipoEqualizacaoTecnicas.show', $id) }}" title="{{ ucfirst( trans('common.show') )}}" class='btn btn-default btn-xs'>--}}
+        {{--<i class="glyphicon glyphicon-eye-open"></i>--}}
+    {{--</a>--}}
     <a href="{{ route('admin.tipoEqualizacaoTecnicas.edit', $id) }}" title="{{ ucfirst( trans('common.edit') )}}" class='btn btn-warning btn-xs'>
         <i class="glyphicon glyphicon-edit"></i>
     </a>
