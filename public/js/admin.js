856d6f4622b7a85483472d6773c53b21a99cc483
Bruno Oliveira <bruno.oliveirati1@gmail.com>
2017-05-05 09:56:20 -0300

    Fazer js (qc-informar-valores.js) executar na página certa
    - Somente na página {id}/informar-valor

--- public/js/admin.js
+++ public/js/admin.js
@@ -64974,96 +64974,108 @@ function workflowAprovaReprova(item_id, tipo_item, aprovou, elemento, nome, pai_
 }
 
 $(function() {
-  var save         = document.getElementById('save');
-  var rows         = document.querySelectorAll('.js-calc-row');
-  var form         = document.getElementById('informar-valores-form');
-  var reject       = document.getElementById('reject');
-  var motivoSelect = document.getElementById('desistencia_motivo_id');
+  console.log('location.href', location.href);
+  console.log('location.href.match(/[0-9]+\/informar-valor/gi)', location.href.match(/[0-9]+\/informar-valor/gi));
+  if(location.href.match(/[0-9]+\/informar-valor/gi)) {
+    QcInformarValoresForm.init();
+  }
+});
 
-  motivoSelect.classList.remove('hidden');
+var QcInformarValoresForm = {
+  init: function() {
+    console.log('QcInformarValoresForm#init()');
 
-  var motivoSelectHtml = motivoSelect.outerHTML;
+    var save         = document.getElementById('save');
+    var rows         = document.querySelectorAll('.js-calc-row');
+    var form         = document.getElementById('informar-valores-form');
+    var reject       = document.getElementById('reject');
+    var motivoSelect = document.getElementById('desistencia_motivo_id');
 
-  motivoSelect.remove();
+    motivoSelect.classList.remove('hidden');
 
-  _.each(rows, function(row) {
-    var price = row.querySelector('.js-calc-price');
-    price.addEventListener('input', function(event) {
-      var price = event.currentTarget;
-      var amount = row.querySelector('.js-calc-amount');
-      var result = row.querySelector('.js-calc-result');
+    var motivoSelectHtml = motivoSelect.outerHTML;
 
-      if(!price.value.length) {
-        result.innerText = 'R$ 0,00';
+    motivoSelect.remove();
 
-        return true;
-      }
+    _.each(rows, function(row) {
+      var price = row.querySelector('.js-calc-price');
+      price.addEventListener('input', function(event) {
+        var price = event.currentTarget;
+        var amount = row.querySelector('.js-calc-amount');
+        var result = row.querySelector('.js-calc-result');
 
-      result.innerText = floatToMoney(
-        parseFloat(amount.innerText, 10) * moneyToFloat(price.value)
-      );
+        if(!price.value.length) {
+          result.innerText = 'R$ 0,00';
+
+          return true;
+        }
+
+        result.innerText = floatToMoney(
+          parseFloat(amount.innerText, 10) * moneyToFloat(price.value)
+        );
+      });
+
+      price.dispatchEvent(new Event('input'));
     });
 
-    price.dispatchEvent(new Event('input'));
-  });
-
-  reject.addEventListener('click', function(event) {
-    event.preventDefault();
-    swal({
-      title: "Rejeitar proposta?",
-      text: '<label for="desistencia_motivo_id">Escolha um motivo</label>' +
+    reject.addEventListener('click', function(event) {
+      event.preventDefault();
+      swal({
+        title: "Rejeitar proposta?",
+        text: '<label for="desistencia_motivo_id">Escolha um motivo</label>' +
         motivoSelect.outerHTML,
-      html: true,
-      type: "input",
-      showCancelButton: true,
-      closeOnConfirm: false,
-      animation: "slide-from-top",
-      inputPlaceholder: "Justificativa",
-      showLoaderOnConfirm: true,
-      cancelButtonText: 'Cancelar',
-      confirmButtonText: 'Rejeitar',
-      confirmButtonColor: '#DD6B55'
-    },
-      function (justificativa_texto) {
-        if (justificativa_texto === false) return false;
+        html: true,
+        type: "input",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        animation: "slide-from-top",
+        inputPlaceholder: "Justificativa",
+        showLoaderOnConfirm: true,
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Rejeitar',
+        confirmButtonColor: '#DD6B55'
+      },
+        function (justificativa_texto) {
+          if (justificativa_texto === false) return false;
 
-        if (!justificativa_texto.length) {
-          swal.showInputError("Escreva uma justificativa!");
-          return false
-        }
-        var motivo = $('#desistencia_motivo_id');
+          if (!justificativa_texto.length) {
+            swal.showInputError("Escreva uma justificativa!");
+            return false
+          }
+          var motivo = $('#desistencia_motivo_id');
 
-        if (!motivo.val().length) {
-          swal.showInputError("Escolha um motivo!");
-          return false
-        }
-        $(form).append($(
-          '<input type="hidden" class="js-additional-input" name="desistencia_texto" value="' + justificativa_texto + '" />' +
-          '<input type="hidden" class="js-additional-input" name="desistencia_motivo_id" value="' + motivo.val()  + '" />' +
-          '<input type="hidden" class="js-additional-input" name="reject" value="1" />'
-        ));
+          if (!motivo.val().length) {
+            swal.showInputError("Escolha um motivo!");
+            return false
+          }
+          $(form).append($(
+            '<input type="hidden" class="js-additional-input" name="desistencia_texto" value="' + justificativa_texto + '" />' +
+            '<input type="hidden" class="js-additional-input" name="desistencia_motivo_id" value="' + motivo.val()  + '" />' +
+            '<input type="hidden" class="js-additional-input" name="reject" value="1" />'
+          ));
 
+          form.submit();
+        });
+    });
+
+    save.addEventListener('click', function(event) {
+      event.preventDefault();
+      swal({
+        title: 'Salvar preços?',
+        text: 'Ao confirmar não será possível voltar atrás',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Salvar',
+        cancelButtonText: 'Cancelar',
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true,
+        confirmButtonColor: '#7ED32C'
+      }, function () {
         form.submit();
       });
-  });
-
-  save.addEventListener('click', function(event) {
-    event.preventDefault();
-    swal({
-      title: 'Salvar preços?',
-      text: 'Ao confirmar não será possível voltar atrás',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Salvar',
-      cancelButtonText: 'Cancelar',
-      closeOnConfirm: false,
-      showLoaderOnConfirm: true,
-      confirmButtonColor: '#7ED32C'
-    }, function () {
-      form.submit();
     });
-  });
-});
+  }
+}
 
 $(function() {
   $('.js-sweetalert').click(function(event) {
