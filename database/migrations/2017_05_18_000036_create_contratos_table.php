0b322082c5ccc3ae4c1e172d45022d0fdd44ebaf
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-05-18 18:17:49 -0300

    BS-180 Modelagem do banco Sprint 3, Criação das Migrations e Models #time 8h Modelagem do banco, Criação das Migrations e Models

---
+++ database/migrations/2017_05_18_000036_create_contratos_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContratosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @table contratos
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->unsignedInteger('contrato_status_id');
+            $table->unsignedInteger('obra_id');
+            $table->unsignedInteger('quadro_de_concorrencia_id')->nullable();
+            $table->unsignedInteger('fornecedor_id');
+            $table->decimal('valor_total', 19, 2);
+            $table->unsignedInteger('contrato_template_id');
+            $table->string('arquivo', 255)->nullable();
+            $table->nullableTimestamps();
+
+
+            $table->foreign('contrato_status_id')
+                ->references('id')->on('contrato_status')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+
+            $table->foreign('obra_id')
+                ->references('id')->on('obras')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('quadro_de_concorrencia_id')
+                ->references('id')->on('quadro_de_concorrencias')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+
+            $table->foreign('fornecedor_id')
+                ->references('id')->on('fornecedores')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+
+            $table->foreign('contrato_template_id')
+                ->references('id')->on('contrato_templates')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists('contratos');
+     }
+}
