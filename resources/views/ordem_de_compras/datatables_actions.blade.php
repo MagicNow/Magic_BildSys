848fc110cf0dd736e14591567f21076b79877b86
Raul Martinez <raul.martinez@fcmoreno.com.br>
2017-05-19 18:14:59 -0300

    BS-227 [ORDEM DE COMPRAS] - ORDEM DE COMPRA LISTAGEM E FILTROS

--- resources/views/ordem_de_compras/datatables_actions.blade.php
+++ resources/views/ordem_de_compras/datatables_actions.blade.php
@@ -1,17 +1,3 @@
-{!! Form::open(['route' => ['ordens-de-compras.destroy', $id], 'id'=>'formDelete'.$id, 'method' => 'delete']) !!}
-<div class='btn-group'>
-    <a href="{{ route('ordens-de-compras.show', $id) }}" title="{{ ucfirst( trans('common.show') )}}" class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-eye-open"></i>
-    </a>
-    <a href="{{ route('ordens-de-compras.edit', $id) }}" title="{{ ucfirst( trans('common.edit') )}}" class='btn btn-warning btn-xs'>
-        <i class="glyphicon glyphicon-edit"></i>
-    </a>
-    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
-            'type' => 'button',
-            'class' => 'btn btn-danger btn-xs',
-            'onclick' => "confirmDelete('formDelete".$id."');",
-            'title' => ucfirst(trans('common.delete'))
-        ]) !!}
-
-</div>
-{!! Form::close() !!}
+<a href="ordens-de-compra/detalhes/{{$id}}"><button type="button" class="btn btn-ms btn-flat">
+    <i class="fa fa-eye" aria-hidden="true"></i>
+</button></a>
