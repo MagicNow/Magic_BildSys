69f900468756925b4fe08a6beed514c5e42a0863
Bruno Oliveira <bruno.oliveira@fcmoreno.com.br>
2017-05-29 16:45:29 -0300

    Alterar validação de worflow de contrato

--- app/Repositories/Admin/WorkflowAlcadaRepository.php
+++ app/Repositories/Admin/WorkflowAlcadaRepository.php
@@ -6,6 +6,7 @@ use Flash;
 use App\Models\WorkflowAlcada;
 use InfyOm\Generator\Common\BaseRepository;
 use Illuminate\Http\Exception\HttpResponseException;
+use App\Models\WorkflowTipo;
 
 class WorkflowAlcadaRepository extends BaseRepository
 {
@@ -28,8 +29,8 @@ class WorkflowAlcadaRepository extends BaseRepository
 
     public function validateBusinessLogic($input, $id = null)
     {
-        if(!empty(@$input['valor_minimo'])) {
-            $input['valor_minimo'] = money_to_float($input['valor_minimo']);
+        if($id) {
+            $alcada = $this->model->find($id);
         }
 
         $alcadas = $this->model
@@ -37,40 +38,47 @@ class WorkflowAlcadaRepository extends BaseRepository
             ->where('id', '!=', $id)
             ->get();
 
+        if(!empty(@$input['valor_minimo'])) {
+            $input['valor_minimo'] = money_to_float($input['valor_minimo']);
+        }
+
         if($alcadas->count()) {
-            $alcada_anterior = $alcadas->where('ordem', '<', intval($input['ordem']))
-                ->first();
+            if(!isset($alcada) || isset($alcada) && $alcada->ordem != $input['ordem']) {
+                $alcada_anterior = $alcadas->where('ordem', '<=', intval($input['ordem']))
+                    ->first();
+                if(!$alcada_anterior) {
+                    Flash::error('Ordem inválida, não há alçadas anteriores.');
 
-            if(!$alcada_anterior) {
-                Flash::error('Ordem inválida, não há alçadas anteriores.');
-
-                throw new HttpResponseException(back()->withInput());
+                    throw new HttpResponseException(back()->withInput());
+                }
             }
 
-            $alcada_igual = $alcadas->where('valor_minimo', $input['valor_minimo'])->first();
+            if($input['workflow_tipo_id'] == WorkflowTipo::CONTRATO) {
+                $alcada_igual = $alcadas->where('valor_minimo', $input['valor_minimo'])->first();
 
-            if($alcada_igual) {
-                Flash::error('Já existe uma alçada com o valor mínimo informado');
+                if($alcada_igual) {
+                    Flash::error('Já existe uma alçada com o valor mínimo informado');
 
-                throw new HttpResponseException(back()->withInput());
-            }
+                    throw new HttpResponseException(back()->withInput());
+                }
 
-            $alcada_anterior_valor_maior = $alcadas->where('ordem', '<', $input['ordem'])
-                ->where('valor_minimo', '>', $input['valor_minimo'])
-                ->first();
+                $alcada_anterior_valor_maior = $alcadas->where('ordem', '<', $input['ordem'])
+                    ->where('valor_minimo', '>', $input['valor_minimo'])
+                    ->first();
 
-            if($alcada_anterior_valor_maior) {
-                Flash::error(
-                    'Existe uma alçada anterior com um valor maior que o utilizado'
-                );
+                if($alcada_anterior_valor_maior) {
+                    Flash::error(
+                        'Existe uma alçada anterior com um valor maior que o utilizado'
+                    );
 
-                throw new HttpResponseException(back()->withInput());
+                    throw new HttpResponseException(back()->withInput());
+                }
             }
 
         } else {
             $input['ordem'] = 1;
 
-            if($input['valor_minimo'] > 0) {
+            if($input['workflow_tipo_id'] == WorkflowTipo::CONTRATO && $input['valor_minimo'] > 0) {
                 Flash::error(
                     'É necessário que a primeira alçada tenha valor mínimo de R$0,00'
                 );
