b7f63bc411fc817a6629e026d6844a6c9b590976
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-06-21 13:32:44 -0300

    BS-351 Item 238 - Ajustado a ordenação das listas suspensas.

--- app/Http/Controllers/CatalogoContratoController.php
+++ app/Http/Controllers/CatalogoContratoController.php
@@ -339,6 +339,7 @@ class CatalogoContratoController extends AppBaseController
             DB::raw("CONVERT(agn_st_nome,'UTF8','WE8ISO8859P15') as agn_st_nome")
         ])
         ->whereRaw("LOWER(agn_st_nome) LIKE '%' || LOWER('{$request->q}') || '%'")
+        ->orderBy('agn_st_nome', 'ASC')
         ->paginate();
 
         return $fornecedores;
@@ -357,6 +358,7 @@ class CatalogoContratoController extends AppBaseController
         ->whereHas('grupo', function($query) {
             return $query->where('active', 1);
         })
+        ->orderBy('nome', 'ASC')
         ->paginate();
 
         return $insumos;
