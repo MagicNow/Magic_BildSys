e80393463740b15ea76d46231cd955c880222b37
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-07-04 15:15:06 -0300

    BS-382 Tabela para preenchimento.

--- resources/views/contratos/memoria_de_calculo/previsao.blade.php
+++ resources/views/contratos/memoria_de_calculo/previsao.blade.php
@@ -51,5 +51,75 @@
             </a>
             {!! Form::select('memoria_de_calculo', $memoria_de_calculo , null, ['class' => 'form-control select2', 'required' => 'required']) !!}
         </div>
+
+        <table class="table table-striped table-no-margin">
+            <thead>
+            <tr>
+                <th>Torre</th>
+                <th>Estrutura</th>
+                <th>Pavimento</th>
+                <th>Trecho</th>
+                <th style="width: 15%;">Data</th>
+                <th style="width: 15%;">Qtde</th>
+                <th style="width: 15%;">%</th>
+                <th style="width: 4%;"></th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+
     </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    var count = 0;
+
+    // Função para adicionar linha na tabela
+    function adicionarNaTabela() {
+        count ++;
+        $('tbody').append('\
+        <tr id=linha_'+count+'>\
+            <td>\
+                torre 1\
+            </td>\
+            <td>\
+                Pre Tipo\
+            </td>\
+            <td>\
+                SS 1\
+            </td>\
+            <td>\
+                Trecho 1\
+            </td>\
+            <td>\
+                {{Form::date('data_competencia', null, ['class' => 'form-control'])}}\
+            </td>\
+            <td>\
+                {{Form::text('qtd', null, ['class' => 'form-control money'])}}\
+            </td>\
+            <td>\
+                {{Form::text('percentual', null, ['class' => 'form-control money'])}}\
+            </td>\
+            <td>\
+            <button onclick="removerLinha('+count+');"class="btn btn-flat btn-sm btn-danger pull-right" data-toggle="tooltip" data-placement="top" title="Remover">\
+                <i class="fa fa-remove fa-fw" aria-hidden="true"></i>\
+            </td>\
+        </tr>\
+        ');
+
+        $('.money').maskMoney({
+            allowNegative: true,
+            thousands: '.',
+            decimal: ','
+        });
+    }
+
+    // Função para remover linha da tabela
+    function removerLinha(id) {
+        $('#linha_'+id).remove();
+    }
+</script>
 @endsection
