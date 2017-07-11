79c45e253c4cdf5a448458dc7181ea4e62899ede
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-07-11 14:36:12 -0300

    BS-385 Item 172 - Ajustado.

--- resources/views/tipo_equalizacao_tecnicas/fields.blade.php
+++ resources/views/tipo_equalizacao_tecnicas/fields.blade.php
@@ -32,7 +32,11 @@
                                     {!! Form::label('itens['.$qtdItens.'][nome]', 'Nome:') !!}
                                     {!! Form::text('itens['.$qtdItens.'][nome]', $item->nome, ['class' => 'form-control']) !!}
                                 </div>
-
+                                <div class="form-group col-sm-1">
+                                    <button type="button" onclick="remExtra({{$qtdItens}},'item_')" class="btn btn-danger flat" style="margin-top: 24px" title="Remover">
+                                        <i class="fa fa-times"></i>
+                                    </button>
+                                </div>
                                 <div class="form-group col-sm-11">
                                     {!! Form::label('itens['.$qtdItens.'][descricao]', 'Descrição:') !!}
                                     {!! Form::textarea('itens['.$qtdItens.'][descricao]', $item->descricao, ['class' => 'form-control', 'rows'=>3]) !!}
@@ -49,11 +53,7 @@
                                 {!! Form::hidden('itens['.$qtdItens.'][id]',$item->id) !!}
                                 {!! Form::hidden('itens['.$qtdItens.'][tipo_equalizacao_tecnica_id]',$item->tipo_equalizacao_tecnica_id) !!}
 
-                                <div class="form-group col-sm-1">
-                                    <button type="button" onclick="remExtra({{$qtdItens}},'item_')" class="btn btn-danger" style="margin-top: 24px" title="Remover">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </div>
+                                <div class="col-md-12 border-separation" style="margin-bottom:20px;"></div>
                             </div>
                         @endforeach
                     @endif
@@ -79,7 +79,7 @@
                                     {!! Form::text('anexos['.$qtdanexos.'][nome]', $anexo->nome, ['class' => 'form-control']) !!}
                                 </div>
                                 <div class="form-group col-sm-1">
-                                    <button type="button" onclick="remExtra({{$qtdanexos}},'arquivo_')" class="btn btn-danger" style="margin-top: 24px" title="Remover">
+                                    <button type="button" onclick="remExtra({{$qtdanexos}},'arquivo_')" class="btn btn-danger flat" style="margin-top: 24px" title="Remover">
                                         <i class="fa fa-times"></i>
                                     </button>
                                 </div>
@@ -97,6 +97,7 @@
                                     </a>
                                     {!! Form::file('anexos['.$qtdanexos.'][arquivo]', null, ['class' => 'form-control','placeholder'=>'Alterar']) !!}
                                 </div>
+                                <div class="col-md-12 border-separation" style="margin-bottom:20px;"></div>
                             </div>
                             <hr>
                         @endforeach
@@ -126,6 +127,7 @@
                     '<label for="itens['+qtditens+'][nome]">Nome:</label>'+
                     '<input class="form-control" name="itens[' + qtditens + '][nome]" type="text" id="itens['+qtditens+'][nome]" required="required" />'+
                     '</div>'+
+                    '<div class="form-group col-sm-1"><button type="button" onclick="remExtra('+qtditens+',\'item_\')" class="btn btn-danger flat" style="margin-top: 24px" title="Remover"><i class="fa fa-times"></i></button> </div>'+
                     '<div class="form-group col-sm-11">'+
                     '<label for="itens['+qtditens+'][descricao]">Descrição:</label>'+
                     '<textarea class="form-control" name="itens[' + qtditens + '][descricao]" type="text" id="itens['+qtditens+'][descricao]"/></textarea>'+
@@ -134,7 +136,7 @@
                     '<label for="itens['+qtditens+'][obrigatorio]">Obrigatório: </label> '+
                     '<input class="icheckbox_square-green hover" type="checkbox" name="itens[' + qtditens + '][obrigatorio]"><br>'+
                     '</div>'+
-                    '<div class="form-group col-sm-1"><button type="button" onclick="remExtra('+qtditens+',\'item_\')" class="btn btn-danger" style="margin-top: 24px" title="Remover"><i class="fa fa-times"></i></button> </div>'+
+                    '<div class="col-md-12 border-separation" style="margin-bottom:20px;"></div>'+
                     '</div>');
         }
 
@@ -145,11 +147,12 @@
                     '<label for="anexos['+qtdanexos+'][nome]">Nome:</label>'+
                     '<input class="form-control" name="anexos[' + qtdanexos + '][nome]" type="text"/>'+
                     '</div>'+
+                    '<div class="form-group col-sm-1"><button type="button" onclick="remExtra('+qtdanexos+',\'arquivo_\')" class="btn btn-danger flat" style="margin-top: 24px" title="Remover"><i class="fa fa-times"></i></button> </div>'+
                     '<div class="form-group col-sm-11">'+
                     '<label for="anexos['+qtdanexos+'][arquivo]">Imagem:</label>'+
                     '<input class="form-control" name="anexos[' + qtdanexos + '][arquivo]" type="file" required="required" />'+
                     '</div>'+
-                    '<div class="form-group col-sm-1"><button type="button" onclick="remExtra('+qtdanexos+',\'arquivo_\')" class="btn btn-danger" style="margin-top: 24px" title="Remover"><i class="fa fa-times"></i></button> </div>'+
+                    '<div class="col-md-12 border-separation" style="margin-bottom:20px;"></div>'+
                     '</div>');
         }
 
