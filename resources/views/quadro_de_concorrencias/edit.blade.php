cb9b5f0fdff513e5df8f2f7f968bc8bfe4d1539b
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-07-13 14:15:20 -0300

    BS-387 Item 226 - Ter um botão para voltar na lista de Q.C.

--- resources/views/quadro_de_concorrencias/edit.blade.php
+++ resources/views/quadro_de_concorrencias/edit.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <section class="content-header">
         <h1>
+            <button type="button" class="btn btn-link" onclick="history.go(-1);">
+                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+            </button>
             Quadro de concorrência {{ $quadroDeConcorrencia->id }}
             <small class="label label-default pull-right margin10">
                 <i class="fa fa-clock-o"
