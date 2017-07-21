ca4921a01cb2a92d583ff965182822c9cf48bd0f
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-07-20 22:22:14 -0300

    BS-392 - Em andamento

---
+++ database/migrations/2017_07_20_000005_create_medicao_boletim_status_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicaoBoletimStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @table medicao_boletim_status
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicao_boletim_status', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('nome', 45);
+            $table->string('cor', 7)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists('medicao_boletim_status');
+     }
+}
---
+++ database/seeds/MedicaoBoletimStatusTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use App\Models\ContratoStatus;
+
+class ContratoStatusTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('contrato_status')->truncate();
+
+        DB::table('contrato_status')->insert([[
+            'id' => ContratoStatus::EM_APROVACAO,
+            'nome' => 'Em Aprovação',
+            'cor' => '#DEA447'
+        ], [
+            'id' => ContratoStatus::APROVADO,
+            'nome' => 'Aprovado',
+            'cor' => '#2BBD30'
+        ], [
+            'id' => ContratoStatus::REPROVADO,
+            'nome' => 'Reprovado',
+            'cor' => '#CC2910'
+        ], [
+            'id' => ContratoStatus::AGUARDANDO,
+            'nome' => 'Aguardando Liberação',
+            'cor' => '#CCCCCC'
+        ], [
+            'id' => ContratoStatus::ATIVO,
+            'nome' => 'Ativo',
+            'cor' => '#008d4c'
+        ], [
+            'id' => ContratoStatus::CANCELADO,
+            'nome' => 'Cancelado',
+            'cor' => '#dd4b39'
+        ]
+        ]);
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
