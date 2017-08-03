7030077680e1ee6a2378f0c691bde5a30d850c72
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-08-03 20:59:18 -0300

    BS-397 - Adicionado Index para aumentar performance da listagem de tarefas

---
+++ database/migrations/2017_08_03_203107_alter_notification_add_index_to_data.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterNotificationAddIndexToData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table){
+            $sql = 'ALTER TABLE `bildsys`.`notifications` ADD INDEX `notifications_data_index` (`data`(500))';
+            DB::connection()->getPdo()->exec($sql);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table){
+           $table->dropIndex('notifications_data_index');
+        });
+    }
+}
