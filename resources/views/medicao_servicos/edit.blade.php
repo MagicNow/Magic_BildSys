fce36fcd072fcb7d2a10597446b9b8c0565a53ff
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-08-11 19:13:19 -0300

    BS-404 - Ajustes na aprovação de Medições

--- resources/views/medicao_servicos/edit.blade.php
+++ resources/views/medicao_servicos/edit.blade.php
@@ -6,12 +6,24 @@
             <button type="button" class="btn btn-link" onclick="history.go(-1);">
                 <i class="fa fa-arrow-left" aria-hidden="true"></i>
             </button>
-            Editar Medicão de Serviço {{ $medicaoServico->id . ' - Obra '. $medicaoServico->contratoItemApropriacao->contratoItem->contrato->obra->nome }}
+            Editar Medicão de Serviço {!! $medicaoServico->id . ' - Obra '.
+            $medicaoServico->contratoItemApropriacao->contratoItem->contrato->obra->nome . ' ' .
+            !is_null($medicaoServico->aprovado)?($medicaoServico->aprovado?'<span class="label label-success">Aprovado</span>':'<span class="label label-danger">Reprovado</span>'):''  !!}
 
-            @if(!$medicaoServico->finalizado)
-                <a class="btn btn-success btn-flat btn-lg pull-right" href="{{ url('/medicoes/create?contrato_item_apropriacao_id='.$medicaoServico->contrato_item_apropriacao_id.'&medicao_servico_id='.$medicaoServico->id) }}">
-                    Continuar medição <i class="fa fa-arrow-right" aria-hidden="true"></i>
-                </a>
+        @if(!$medicaoServico->finalizado)
+                @if(!$medicaoServico->medicoes()->where('aprovado','0')->count())
+                    <a class="btn btn-success btn-flat btn-lg pull-right" href="{{ url('/medicoes/create?contrato_item_apropriacao_id='.$medicaoServico->contrato_item_apropriacao_id.'&medicao_servico_id='.$medicaoServico->id) }}">
+                        Continuar medição <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                    </a>
+                @else
+                    <button data-toggle="tooltip" data-placement="left"
+                            type="button" class="btn btn-success btn-flat btn-lg pull-right disabled"
+
+                       title="Impossível continuar, pois existem itens reprovados!">
+                        Continuar medição <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                    </button>
+                @endif
+
             @endif
         </h1>
    </section>
