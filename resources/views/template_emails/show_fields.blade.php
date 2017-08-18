98221241b2e67e0cd5d4c166f55971c7aadc6cca
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-08-18 15:19:17 -0300

    BS-419 - Ajuste em exibição de registro

--- resources/views/template_emails/show_fields.blade.php
+++ resources/views/template_emails/show_fields.blade.php
@@ -1,37 +1,35 @@
-<!-- Id Field -->
-<div class="form-group col-md-6">
-    {!! Form::label('id', 'Id:') !!}
-    <p class="form-control">{!! $templateEmail->id !!}</p>
-</div>
-
 <!-- Nome Field -->
-<div class="form-group col-md-6">
+<div class="form-group col-md-12">
     {!! Form::label('nome', 'Nome:') !!}
     <p class="form-control">{!! $templateEmail->nome !!}</p>
 </div>
-
-<!-- Template Field -->
-<div class="form-group col-md-6">
-    {!! Form::label('template', 'Template:') !!}
-    <p class="form-control">{!! $templateEmail->template !!}</p>
-</div>
-
 <!-- Created At Field -->
-<div class="form-group col-md-6">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p class="form-control">{!! $templateEmail->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group col-md-6">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p class="form-control">{!! $templateEmail->updated_at !!}</p>
+<div class="form-group col-md-4">
+    {!! Form::label('created_at', 'Criado em:') !!}
+    <p class="form-control">{!! $templateEmail->created_at->format('d/m/Y H:i') !!}</p>
 </div>
 
 <!-- User Id Field -->
-<div class="form-group col-md-6">
-    {!! Form::label('user_id', 'User Id:') !!}
-    <p class="form-control">{!! $templateEmail->user_id !!}</p>
+<div class="form-group col-md-4">
+    {!! Form::label('user_id', 'Por:') !!}
+    <p class="form-control">{!! $templateEmail->user_id ? $templateEmail->user()->withTrashed()->first()->name : '' !!}</p>
 </div>
+
+<!-- Updated At Field -->
+<div class="form-group col-md-4">
+    {!! Form::label('updated_at', 'Alterado em:') !!}
+    <p class="form-control">{!! $templateEmail->updated_at ? $templateEmail->updated_at->format('d/m/Y H:i'):'' !!}</p>
+</div>
+
+
+<!-- Template Field -->
+<div class="form-group col-md-12">
+    {!! Form::label('template', 'Template:') !!}
+    <div class="well well-sm">{!! $templateEmail->template !!}</div>
+</div>
+
+
+
+
 
 
