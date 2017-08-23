91256cb96cdcdb2920f9b55a78e45ce77f9a232c
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-08-23 15:25:06 -0300

    BS-455 - Ajuste da planilha 367 e 369

--- public/js/admin.js
+++ public/js/admin.js
@@ -66377,3 +66377,14 @@ var NotificationSystem = {
   }
 };
 
+
+/**
+ * Traz o insumo do servidor pelo id
+ *
+ * @param {Number} id
+ *
+ * @return jQuery.promise
+ */
+function getInsumo(id) {
+  return $.get('/insumos/' + id + '/json');
+}
