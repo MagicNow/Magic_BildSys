bf08395cf67175af94c46b4611ad193b11ba4ee5
Phelipe Momesso <phelipe.momesso@gmail.com>
2017-08-23 23:11:40 -0300

    Adjust ConfiguracaoEstaticaTableSeeder with new data - Cabeçalho Bild Matriz

--- database/seeds/ConfiguracaoEstaticaTableSeeder.php
+++ database/seeds/ConfiguracaoEstaticaTableSeeder.php
@@ -29,6 +29,11 @@ class ConfiguracaoEstaticaTableSeeder extends Seeder
                 'id' => 3,
                 'chave' => 'Ciente no boletim da Medição de Serviço',
                 'valor' => 'Declaro concordar com os valores e quantidades constantes nesta medição, não restando nada a medir até esta data, e  estar ciente  da necessidade do envio da documentação descriminada em contrato para a liberação desta medição.'
+            ],
+            [
+                'id' => 4,
+                'chave' => 'Cabeçalho Bild Matriz',
+                'valor' => 'BILD DESENVOLVIMENTO IMOBILIARIO , pessoa jurídica de direito privado, com sede na AV. PROFESSOR JOÃO FIUSA - 2340, Bairro: JD. CANADÁ na cidade de RIBEIRÃO PRETO / SP com o CEP: 14024-260 , inscrita no CNPJ sob nº 08.964.23/0001-50, Inscrição Estadual: 582.884.524.111 e Inscrição Municipal: 12635301 com seu contrato social devidamente registrado na junta Comercial do Estado de São Paulo, neste ato representada na forma de seus atos constitutivos, doravante denominada simplesmente CONTRATANTE.'
             ]
         ];
 
