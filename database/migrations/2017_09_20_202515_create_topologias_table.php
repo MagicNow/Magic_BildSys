f558f2e24830012570e336252f21b65b04d6959b
Heitor Glockner <heitor@magictech.co>
2017-09-20 21:22:00 -0300

    atualiza qc migrations

--- database/migrations/2017_09_20_202515_create_topologias_table.php
+++ database/migrations/2017_09_20_202515_create_topologias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTipologiasTable extends Migration
+class CreateTopologiasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTipologiasTable extends Migration
      */
     public function up()
     {
-        Schema::create('tipologias', function (Blueprint $table) {
+        Schema::create('topologias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
 
@@ -29,6 +29,6 @@ class CreateTipologiasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tipologias');
+        Schema::dropIfExists('topologias');
     }
 }
