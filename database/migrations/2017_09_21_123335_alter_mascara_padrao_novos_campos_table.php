294b2d4a1f2140f0f3ecbab3e417f033b60d8fe5
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-09-22 14:16:38 -0300

    Ajuste na migration.

--- database/migrations/2017_09_21_123335_alter_mascara_padrao_novos_campos_table.php
+++ database/migrations/2017_09_21_123335_alter_mascara_padrao_novos_campos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterMascaraPadraoNocosCamposTable extends Migration
+class AlterMascaraPadraoNovosCamposTable extends Migration
 {
     /**
      * Run the migrations.
