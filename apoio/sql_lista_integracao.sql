a652a0e4388d26a950a618193ad2a53417f2dc4d
Rafael Querino Moreira <rafael.querino@fcmoreno.com.br>
2017-09-28 18:14:53 -0300

    BS-536 - Comentários Para referência

--- apoio/sql_lista_integracao.sql
+++ apoio/sql_lista_integracao.sql
@@ -8,6 +8,8 @@ FROM
 JOIN mgint.int_transacao TRANSACAO ON XML_INTEGRACAO.trn_in_id = TRANSACAO.trn_in_id
 LEFT JOIN mgint.int_transacaolog TRANSACAO_LOG ON XML_INTEGRACAO.trn_in_id = TRANSACAO_LOG.trn_in_id
 WHERE
+  -- TRANSACAO.pro_in_id = 705 -- NF
+  -- TRANSACAO.pro_in_id = 306 -- PAGAMENTO
 	XML_INTEGRACAO.TRN_IN_ID = 100803
 ORDER BY
 	TRANSACAO.trn_dt_datatranscao DESC;
