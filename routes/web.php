004ce93a3b14977e05a9dc88f4a73471151d420c
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-09-28 16:04:24 -0300

    BS-547 QR Code funcionando.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@
 
 Auth::routes();
 
+$router->get('/qrcode', function () {
+    return View('qrcode.qrcode');
+});
+
 // Notifications
 $router->get('/notifications', 'NotificationController@index');
 $router->post('/notifications/{id}/mark-as-read', 'NotificationController@markAsRead');
