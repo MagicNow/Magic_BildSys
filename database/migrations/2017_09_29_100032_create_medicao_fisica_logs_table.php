e3bd13334bee2a0e40a25e69d376349aacbd3f85
luismareze <luis.mareze@fcmoreno.com.br>
2017-09-29 12:11:26 -0300

    BS-530 migration e correcoes

---
+++ database/migrations/2017_09_29_100032_create_medicao_fisica_logs_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicaoFisicaLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @table catalogo_contratos
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicao_fisica_logs', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->unsignedInteger('medicao_fisica_id');
+			$table->unsignedInteger('user_id');
+            $table->string('tarefa', 255)->nullable();
+            $table->date('periodo_inicio')->nullable();
+            $table->date('periodo_termino')->nullable();
+			$table->decimal('valor_medido_anterior', 19, 2);            
+            $table->decimal('valor_medido_atual', 19, 2);            
+			$table->timestamps();
+
+            $table->foreign('medicao_fisica_id')
+                ->references('id')->on('obras')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+			
+			$table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+			
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {        
+       Schema::dropIfExists('medicao_fisicas');
+     }
+}
