ff75ed3bffd6452122da43006afb85e0b3e90eb5
Heitor Glockner <heitor@magictech.co>
2017-10-01 21:20:20 -0300

    qc surimentos list

--- database/migrations/2017_09_29_112659_alter_qc_add_columns.php
+++ database/migrations/2017_09_29_112659_alter_qc_add_columns.php
@@ -17,9 +17,16 @@ class AlterQcAddColumns extends Migration
             $table->unsignedInteger('fornecedor_id')->nullable()->after('user_id');
             $table->decimal('valor_fechamento', 19, 2)->nullable()->after('user_id');;
             $table->string('numero_contrato', 30)->after('user_id');;
+            $table->unsignedInteger('comprador_id')->nullable()->after('user_id');
+            $table->date('data_fechamento');
 
             $table->foreign('fornecedor_id')
                 ->references('id')->on('fornecedores')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('comprador_id')
+                ->references('id')->on('users')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
         });
@@ -34,10 +41,13 @@ class AlterQcAddColumns extends Migration
     {
         Schema::table('qc', function (Blueprint $table) {
             $table->dropForeign(['fornecedor_id']);
+            $table->dropForeign(['comprador_id']);
 
             $table->dropColumn('fornecedor_id');
+            $table->dropColumn('comprador_id');
             $table->dropColumn('valor_fechamento');
             $table->dropColumn('numero_contrato');
+            $table->dropColumn('data_fechamento');
         });
     }
 }
