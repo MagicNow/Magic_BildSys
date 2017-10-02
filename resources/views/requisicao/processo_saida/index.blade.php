35a8ce32329faeccde087fbc97f435d2582c8a9a
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-10-02 19:41:01 -0300

    BS-557 Modal com o local da requisição.

--- resources/views/requisicao/processo_saida/index.blade.php
+++ resources/views/requisicao/processo_saida/index.blade.php
@@ -22,22 +22,22 @@
 
                     <div class="col-md-4"></div>
                     <div class="btn-group-vertical col-md-4" style="display: block;">
-                        <a href="{!! route('requisicao.index') !!}" class="btn btn-success" style="margin-bottom: 10px;">
+                        <a href="#" class="btn btn-success" style="margin-bottom: 10px;">
                             <i class="fa fa-qrcode" aria-hidden="true"></i>
                             Ler QrCodes Saída
                         </a>
 
-                        <a href="{!! route('requisicao.index') !!}" class="btn btn-info" style="margin-bottom: 10px;">
+                        <a href="#" class="btn btn-info" style="margin-bottom: 10px;">
                             <i class="fa fa-list" aria-hidden="true"></i>
                             Lista de Inconsistências
                         </a>
 
-                        <a href="{!! route('requisicao.index') !!}" class="btn btn-primary" style="margin-bottom: 10px;">
+                        <a onclick="localRequisicaoModal();" class="btn btn-primary" style="margin-bottom: 10px;">
                             <i class="fa fa-location-arrow" aria-hidden="true"></i>
                             Local da Requisição
                         </a>
 
-                        <a href="{!! route('requisicao.index') !!}" class="btn btn-warning" style="margin-bottom: 10px;">
+                        <a href="#" class="btn btn-warning" style="margin-bottom: 10px;">
                             <i class="fa fa-sign-out" aria-hidden="true"></i>
                             Finalizar Saída
                         </a>
@@ -53,4 +53,22 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        function localRequisicaoModal() {
+            swal({
+                title: 'Local da Requisição',
+                text: '<div style="text-align: left;" class="col-md-12">'+
+                    '<p><b>Local:</b> {{$requisicao->local}}</p>'+
+                    '<p><b>Torre:</b> {{$requisicao->torre}}</p>'+
+                    '<p><b>Pavimento:</b> {{$requisicao->pavimento}}</p>'+
+                    '<p><b>Trecho:</b> {{$requisicao->trecho}}</p>'+
+                    '<p><b>Andar:</b> {{$requisicao->andar}}</p></div>',
+                html: true,
+                confirmButtonColor: "#DD6B55"
+            });
+        }
+    </script>
 @endsection
