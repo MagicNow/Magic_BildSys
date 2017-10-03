1fb7bac9488d01b5e1cd4cc1b99ebe31d3c43cce
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-10-03 10:56:07 -0300

    BS-555 Ajuste no nome.

--- resources/views/requisicao/processo_saida/index.blade.php
+++ resources/views/requisicao/processo_saida/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Requisicao
+            Processo de saída
         </h1>
     </section>
     <div class="content">
