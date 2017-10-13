ef411d9950940ce8b24d4b2b51406b9a85eb629f
zeraaah <jhonatan.henrique@fcmoreno.com.br>
2017-10-13 18:41:44 -0300

    BS-570 Zoom do site.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,10 @@
     <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
     <link rel="icon" href="{{ asset('img/favicon.png') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     @yield('styles')
     <style type="text/css">
         body {
-            zoom: 100%; /* Webkit browsers */
+            zoom: 75%; /* Webkit browsers */
         }
     </style>
 </head>
