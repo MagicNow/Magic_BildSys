737ddbb00d1eadf2841f868f6078688427cfab35
raul_martinez <raul.martinez@fcmoreno.com.br>
2017-10-17 18:30:20 -0200

    ITEM 350 da planilha levantamento

--- resources/views/medicoes/pre-create.blade.php
+++ resources/views/medicoes/pre-create.blade.php
@@ -6,7 +6,7 @@
             <button type="button" class="btn btn-link" onclick="history.go(-1);">
                 <i class="fa fa-arrow-left" aria-hidden="true"></i>
             </button>
-            Nova Medicão
+            Nova medição
         </h3>
     </section>
     <div class="content">
