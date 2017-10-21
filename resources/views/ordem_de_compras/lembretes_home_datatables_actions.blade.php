1329480e0ea54100a474e107606a247deb0596b8
Heitor Glockner <heitor@magictech.co>
2017-10-21 15:50:06 -0200

    eye size

--- resources/views/ordem_de_compras/lembretes_home_datatables_actions.blade.php
+++ resources/views/ordem_de_compras/lembretes_home_datatables_actions.blade.php
@@ -20,7 +20,7 @@ if($carteira_id) {
     <a href="{{ url($url) }}" class="btn btn-sm btn-flat btn-{{ $alerta }}" style="margin-top: 5px;" title="Visualizar" data-toggle="tooltip" data-placement="top" >
         {{--{!! $icone !!}--}}
         {{--Visualizar--}}
-        {{$dias}} dias <i class="fa fa-eye" aria-hidden="true" style="font-size: 14px;"></i>
+        {{$dias}} dias <i class="fa fa-eye" aria-hidden="true" style="font-size: 10px;"></i>
     </a>
     <a href="javascript:void(0);" onclick="dispensarInsumos('{{ $url_dispensar }}', 'Deseja realmente dispensar?')" class="btn btn-sm btn-flat btn-warning" style="margin-top: 5px;" title="Dispensar lembrete" data-toggle="tooltip" data-placement="top" >
         <i class="fa fa-times" aria-hidden="true"></i>
